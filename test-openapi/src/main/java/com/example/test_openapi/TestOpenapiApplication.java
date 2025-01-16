32e402597282c13a08375a9a6d27ad8dfe0e2602
phucnguyen <phucnguyen@futurify.vn>
2025-01-16 10:29:20 +0700

    core

---
+++ test-openapi/src/main/java/com/example/test_openapi/TestOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOpenapiApplication.class, args);
+	}
+
+}
---
+++ test-openapi/src/test/java/com/example/test_openapi/TestOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
